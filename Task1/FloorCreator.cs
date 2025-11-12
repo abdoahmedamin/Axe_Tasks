7c728d918863457b5c4ff676ba02f3943270c783
Abdelrahman Ahmed Amin <abdoahmedamin@gmail.com>
2025-11-13 01:10:20 +0200

    //Task1

--- Task1/FloorCreator.cs
+++ Task1/FloorCreator.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace Task1
 {
@@ -17,8 +18,40 @@ namespace Task1
             UIDocument uIDocument = commandData.Application.ActiveUIDocument;
             Document document = uIDocument.Document;
 
-            TaskDialog.Show("Success", "Floor created successfully!");
-            return Result.Succeeded;
+            try
+            {
+                List<Line> lines = new List<Line>
+                {
+                    Line.CreateBound(new XYZ(0, 0, 0), new XYZ(79, 0, 0)),
+                    Line.CreateBound(new XYZ(44, 25, 0), new XYZ(13, 25, 0)),
+                    Line.CreateBound(new XYZ(13, 40, 0), new XYZ(-8, 40, 0)),
+                    Line.CreateBound(new XYZ(55, 34, 0), new XYZ(55, 10, 0)),
+                    Line.CreateBound(new XYZ(79, 34, 0), new XYZ(55, 34, 0)),
+                    Line.CreateBound(new XYZ(0, 20, 0), new XYZ(0, 0, 0)),
+                    Line.CreateBound(new XYZ(55, 10, 0), new XYZ(44, 12, 0)),
+                    Line.CreateBound(new XYZ(-8, 40, 0), new XYZ(-8, 20, 0)),
+                    Line.CreateBound(new XYZ(79, 0, 0), new XYZ(79, 34, 0)),
+                    Line.CreateBound(new XYZ(44, 12, 0), new XYZ(44, 25, 0)),
+                    Line.CreateBound(new XYZ(-8, 20, 0), new XYZ(0, 20, 0)),
+                    Line.CreateBound(new XYZ(13, 25, 0), new XYZ(13, 40, 0))
+                };
+
+                
+
+                return Result.Succeeded;
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                TaskDialog.Show("Error", "OOPS ! error occurred: " + ex.Message);
+                return Result.Failed;
+            }
         }
+
+
+
+
+
+
     }
 }
