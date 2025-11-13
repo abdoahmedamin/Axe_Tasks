3e1aa92b70f568e222f021e63e2c6fc06be97884
Abdelrahman Ahmed Amin <abdoahmedamin@gmail.com>
2025-11-13 03:28:19 +0200

    //get bathrooms that intersect with the wall

--- Task2/ProperInRoomPlacement.cs
+++ Task2/ProperInRoomPlacement.cs
@@ -16,6 +16,9 @@ namespace Task2
     [Transaction(TransactionMode.Manual)]
     public class ProperInRoomPlacement
     {
+        string familyTypeName = "ADA";
+        string roomName = "Bathroom";
+
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
             UIDocument uIDocument = commandData.Application.ActiveUIDocument;
@@ -23,7 +26,31 @@ namespace Task2
 
             try
             {
-               
+                Reference wallRef = uIDocument.Selection.PickObject(ObjectType.Element, new SelectionFilter("Walls"), "Pick a Wall");
+                Wall wall = document.GetElement(wallRef) as Wall;
+
+                //Get the suitable family symbol in the document
+                FamilySymbol familySymbol = new FilteredElementCollector(document).OfClass(typeof(FamilySymbol))
+                                                                .Cast<FamilySymbol>()
+                                                                .Where(e => e.Name == familyTypeName)
+                                                                .FirstOrDefault();
+
+                if (familySymbol == null)
+                {
+                    TaskDialog.Show("Error", $"Family Type '{familyTypeName}' not found.");
+                    return Result.Failed;
+                }
+
+                // wall is boundry segment in ?? rooms
+                BoundingBoxXYZ wallbb = wall.get_BoundingBox(null);
+                Outline outline = new Outline(wallbb.Min, wallbb.Max);
+                BoundingBoxIntersectsFilter boundingBoxIntersectsFilter = new BoundingBoxIntersectsFilter(outline);
+
+                // all rooms with name contains Bathroom and intersext with the wall
+                List<Room> rooms = new FilteredElementCollector(document, document.ActiveView.Id).OfCategory(BuiltInCategory.OST_Rooms)
+                                                                    .WherePasses(boundingBoxIntersectsFilter)
+                                                                    .Cast<Room>()
+                                                                    .Where(r => r.Name.Contains(roomName)).ToList();
 
                 return Result.Succeeded;
             }
