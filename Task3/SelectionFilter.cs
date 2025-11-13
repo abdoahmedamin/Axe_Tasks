563036bf571c70aef270c0bb4dcb1e14d087b38c
Abdelrahman Ahmed Amin <abdoahmedamin@gmail.com>
2025-11-13 06:44:01 +0200

    //Add SelectionFilter Class

---
+++ Task3/SelectionFilter.cs
@@ -0,0 +1,28 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    public class SelectionFilter : ISelectionFilter
+    {
+        private string categoryName;
+        public SelectionFilter(string categoryName)
+        {
+            this.categoryName = categoryName;
+        }
+        public bool AllowElement(Element elem)
+        {
+            return elem.Category != null && elem.Category.Name == categoryName;
+        }
+
+        public bool AllowReference(Reference reference, XYZ position)
+        {
+            return false;
+        }
+    }
+}
