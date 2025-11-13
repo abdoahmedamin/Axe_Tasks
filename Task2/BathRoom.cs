342337ab837f7fe9ceb573f1262b0576dffa28a2
Abdelrahman Ahmed Amin <abdoahmedamin@gmail.com>
2025-11-13 02:49:16 +0200

    BathRoom Class

---
+++ Task2/BathRoom.cs
@@ -0,0 +1,25 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class BathRoom
+    {
+        public string Name { get; set; }
+
+        public Curve WallSegment { get; set; }
+
+        public XYZ DoorLocation { get; set; } = null;
+
+        public XYZ Center { get; set; }
+
+        public BathRoom(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Task2/RevitApp.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Media.Imaging;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace Task2
+{
+    public class RevitApp : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            try
+            {
+                application.CreateRibbonTab("AXERevit");
+            }
+            catch (Exception) { }
+
+            RibbonPanel panel = null;
+            try
+            {
+                panel = application.GetRibbonPanels("AXERevit").FirstOrDefault(p => p.Name == "Tools");
+            }
+            catch (Exception) { }
+
+            if (panel == null)
+            {
+                panel = application.CreateRibbonPanel("AXERevit", "Tools");
+            }
+
+            string assemblyPath = Assembly.GetExecutingAssembly().Location;
+            var buttonData = new PushButtonData(
+                "Task2",
+                "Task2\nProper InRoom Placement",
+                assemblyPath,
+                typeof(ProperInRoomPlacement).FullName);
+
+            Uri iconUri = new Uri("pack://application:,,,/Task2;component/Resources/toilet.ico");
+            var buttonImage = new BitmapImage(iconUri);
+            buttonData.LargeImage = buttonImage;
+
+
+            var button = panel.AddItem(buttonData) as PushButton;
+            button.ToolTip = "Proper InRoom Placement";
+
+            return Result.Succeeded;
+        }
+    }
+}
