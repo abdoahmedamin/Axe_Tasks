afa2eba2a1d151ed67e60fbbf012e474f9c3eb20
Abdelrahman Ahmed Amin <abdoahmedamin@gmail.com>
2025-11-13 15:09:54 +0200

    // wall face

--- Task4/FramingWallCommand.cs
+++ Task4/FramingWallCommand.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,52 @@ using System.Threading.Tasks;
 
 namespace Task4
 {
-    public class FramingWallCommand
+    public class FramingWallCommand : IExternalCommand
     {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIDocument uiDocument = commandData.Application.ActiveUIDocument;
+            Document document = uiDocument.Document;
+
+
+            try
+            {
+
+                Reference wallRef = uiDocument.Selection.PickObject(ObjectType.Element, new SelectionFilter("Walls"), "Pick a Wall");
+                Wall wall = document.GetElement(wallRef) as Wall;
+
+                Solid wallSolid = GetWallSolid(wall);
+                if (wallSolid == null)
+                {
+                    message = "Failed to get wall geometry.";
+                    return Result.Failed;
+                }
+
+                // wall face
+                Face wallFace = wallSolid.Faces.Cast<Face>().OrderByDescending(f => f.Area).FirstOrDefault();
+                XYZ wallNormal = wallFace.ComputeNormal(new UV(0.5, 0.5));
+                IList<CurveLoop> curveLoops = wallFace.GetEdgesAsCurveLoops();
+
+
+            }
+            catch
+            {
+
+            }
+        }
+
+        #region Method
+        private Solid GetWallSolid(Wall wall)
+        {
+            Options options = new Options();
+            options.ComputeReferences = true;
+            GeometryElement wallGeometry = wall.get_Geometry(options);
+
+            return wallGeometry
+                .Cast<GeometryObject>()
+                .OfType<Solid>()
+                .FirstOrDefault(solid => solid.Volume > 0);
+        }
+        #endregion
     }
 }
